0b6bc86fe6db50882989acf3f421c8f4bed2b5b7
stormy Adams <stormy251@users.noreply.github.com>
2021-08-16 08:39:23 -0700

    Updating the app to have the geist component library and styled components SSR ready

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
-import '../../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type {AppProps} from 'next/app';
+import {GeistProvider, CssBaseline} from '@geist-ui/react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}: AppProps) {
+  return (
+    <GeistProvider themeType="dark">
+      <CssBaseline />
+      <Component {...pageProps} />
+    </GeistProvider>
+  );
 }
-export default MyApp
+export default MyApp;
