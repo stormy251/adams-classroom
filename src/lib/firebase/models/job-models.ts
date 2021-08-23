7a722d6d5b56a837ed18d010ad3f6be5b8d5d4a8
stormy Adams <stormy251@users.noreply.github.com>
2021-08-23 06:56:38 -0700

    save progress

---
+++ src/lib/firebase/models/job-models.ts
@@ -0,0 +1,4 @@
+export type Job = {
+  name: string;
+  count: number;
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,30 @@
 import type {NextPage} from 'next';
 import Head from 'next/head';
-import {Button, Page} from '@geist-ui/react';
+import {Button, Page, Text, Spacer} from '@geist-ui/react';
 import {studentsFetcher} from 'lib/firebase/fetchers/students-fetchers';
+import {jobsFetcher} from 'lib/firebase/fetchers/jobs-fetchers';
+import {useEffect, useState} from 'react';
+import {Student} from 'lib/firebase/models/student-models';
+import {Job} from 'lib/firebase/models/job-models';
 
 const Home: NextPage = () => {
-  const test = async () => {
-    const myStudents = await studentsFetcher();
-    console.log('myStudents:', myStudents);
+  const [isLoadingInfo, setIsLoadingInfo] = useState(true);
+  const [students, setStudents] = useState<Student[]>([]);
+  const [jobs, setJobs] = useState<Job[]>([]);
+
+  const loadStudentsAndJobs = async () => {
+    const latestStudents = await studentsFetcher();
+    setStudents(latestStudents);
+    const latestJobs = await jobsFetcher();
+    setJobs(latestJobs);
+    if (isLoadingInfo) {
+      setIsLoadingInfo(false);
+    }
   };
+
+  useEffect(() => {
+    loadStudentsAndJobs();
+  }, []);
 
   return (
     <Page>
@@ -20,12 +37,33 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Page.Header>
-        <h2>Header</h2>
+        <h2>Student Jobs</h2>
       </Page.Header>
       <Page.Content>
-        <h2>Hello, Everyone.</h2>
+        <div style={{display: 'flex'}}>
+          <div>
+            <Text h3>Students</Text>
+            {!isLoadingInfo &&
+              students &&
+              students.map((student, index) => (
+                <Text key={`${student.name}-${index + 1}`}>
+                  {index + 1}. {student.name}
+                </Text>
+              ))}
+          </div>
+          <Spacer w={2} />
+          <div>
+            <Text h3>Jobs</Text>
+            {!isLoadingInfo &&
+              jobs &&
+              jobs.map((job, index) => (
+                <Text key={`${job.name}-${index + 1}`}>
+                  {index + 1}. {job.name}
+                </Text>
+              ))}
+          </div>
+        </div>
         <p>This is a simulated page, you can click anywhere to close it.</p>
-        <Button onClick={() => test()}>Test</Button>
       </Page.Content>
       <Page.Footer>
         <h2>Footer</h2>
